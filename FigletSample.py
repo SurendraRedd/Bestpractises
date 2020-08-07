883bcdc1ab347f2ed47c4fecf58c6f7d036d9e81
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2020-08-07 06:39:59 +0530

    Figlet Template

---
+++ FigletSample.py
@@ -0,0 +1,64 @@
+# import pyfiglet module 
+import pyfiglet
+
+# Syntax - pyfiglet.figlet_format(text, font)
+
+# Default Font - 'Standard'
+result = pyfiglet.figlet_format("TextBanners") 
+print(result)
+
+# Slant font text
+result = pyfiglet.figlet_format("Text Banners", font = "slant"  ) 
+print(result)
+
+# 3-D font Text
+result = pyfiglet.figlet_format("T e x t B a n n e r s", font = "3-d" ) 
+print(result)
+
+# 3×5 font Text
+result = pyfiglet.figlet_format("T e x t B a n n e r s", font = "3x5" ) 
+print(result)
+
+# 5lineoblique font Text
+result = pyfiglet.figlet_format("T e x t B a n n e r s", font = "5lineoblique" ) 
+print(result)
+
+# alphabet font Text
+result = pyfiglet.figlet_format("T e x t B a n n e r s", font = "alphabet" ) 
+print(result)
+
+# banner3-D font Text
+result = pyfiglet.figlet_format("TextBanners", font = "banner3-D" ) 
+print(result)
+
+# doh font Text
+result = pyfiglet.figlet_format("TextBanners", font = "doh" ) 
+print(result) 
+
+# isometric1 font Text
+result = pyfiglet.figlet_format("TextBanners", font = "isometric1" ) 
+print(result)
+
+#  letters font text
+result = pyfiglet.figlet_format("TextBanners", font = "letters" ) 
+print(result)
+
+# alligator font text
+result = pyfiglet.figlet_format("T e x t B a n n e r s", font = "alligator" ) 
+print(result)
+
+# dot matrix font text
+result = pyfiglet.figlet_format("TextBanners", font = "dotmatrix" ) 
+print(result)
+
+# bubble font text
+result = pyfiglet.figlet_format("Text Banners", font = "bubble" ) 
+print(result)
+
+# bubblehead font text
+result = pyfiglet.figlet_format("Text Banners", font = "bulbhead" ) 
+print(result)
+
+# digital font text
+result = pyfiglet.figlet_format("Text Banners", font = "digital" ) 
+print(result)
